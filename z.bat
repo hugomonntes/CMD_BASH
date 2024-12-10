2a6baa325835b9200fc8e29d0469c45f1a5d90b2
Hugo Montes <132196565+hugomonntes@users.noreply.github.com>
2024-12-10 12:32:25 +0100

    Create z.bat

---
+++ z.bat
@@ -0,0 +1,26 @@
+ALLUSERSPROFILE=C:\ProgramData                                                                           
+APPDATA=C:\Users\Hugo Montes\AppData\Roaming                                                          
+AppsRoot=D                                                                                                  
+CommonProgramFiles=C:\Program Files\Common Files                                                             
+CommonProgramFiles(x86)=C:\Program Files (x86)\Common Files                                           
+CommonProgramW6432=C:\Program Files\Common Files                                                           
+COMPUTERNAME=DESKTOP-S944IHJ                                        
+ComSpec=C:\WINDOWS\system32\cmd.exe                                                                   
+DriverData=C:\Windows\System32\Drivers\DriverData                                                            
+FPS_BROWSER_APP_PROFILE_STRING=Internet Explorer                                                             
+FPS_BROWSER_USER_PROFILE_STRING=Default                                                                     
+HOMEDRIVE=C:                                                                                                 
+HOMEPATH=\Users\Hugo Montes                                                                                  
+JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-11.0.24.8-hotspot\                                            
+LOCALAPPDATA=C:\Users\Hugo Montes\AppData\Local                                                              
+LOGONSERVER=\\DESKTOP-S944IHJ                                                                                
+NUMBER_OF_PROCESSORS=4                                                                                       
+OneDrive=C:\Users\HALLUSERSPROFILE=C:\ProgramData                                                             
+APPDATA=C:\Users\Hugo Montes\AppData\Roaming                                                               
+AppsRoot=D                                                                                                 
+CommonProgramFiles=C:\Program Files\Common Files                                                            
+CommonProgramFiles(x86)=C:\Program Files (x86)\Common Files                                                 
+CommonProgramW6432=C:\Program Files\Common Files                                                         
+COMPUTERNAME=DESKTOP-S944IHJ                                                         
+OneDrive=C:\Users\H
+
