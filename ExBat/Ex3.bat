6c9f040d8e5a86ef403584b173e10de78b41bbb9
Hugo Montes <132196565+hugomonntes@users.noreply.github.com>
2025-02-03 17:29:34 +0100

    Create Ex3.bat

---
+++ ExBat/Ex3.bat
@@ -0,0 +1,12 @@
+3. a) Realiza un fichero denominado ARBOL.BAT que cree el árbol de directorios siguiente:
+TEMP
+├───C
+│ ├───ejercici
+│ │ ├───nuevos
+│ │ └───revisado
+│ └───programa
+├───datos
+├───princip
+└───result
+b) Haz un fichero denominado ARBOL2.BAT que permita crear el árbol anterior en el lugar indicado como
+parámetro.
