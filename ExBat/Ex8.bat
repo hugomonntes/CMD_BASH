f42cb149074b5009541c2c8b50b803c98dca6ea5
Hugo Montes <132196565+hugomonntes@users.noreply.github.com>
2025-02-07 12:41:23 +0100

    Update Ex8.bat

--- ExBat/Ex8.bat
+++ ExBat/Ex8.bat
@@ -1,3 +1,4 @@
+a)
 @echo off
 cls
 
@@ -8,3 +9,20 @@ if "%1"=="" (
 
 echo Buscando "%~1" en C:\
 dir C:\%~1 /s /p
+
+b)
+@echo off
+cls
+
+if "%1"=="" (
+	echo Archivo a buscar:
+	exit /b
+)
+
+if "%2"=="" (
+	echo Unidad donde buscar:
+	exit /b
+)
+
+echo Buscando... "%1" en "%2"...
+dir %2\%1 /s /p
